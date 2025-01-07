8db87f5e2f0e0ea4ace650a4ca308d3fcb253d3e
Afzal khan <afzalkhan01765@gmail.com>
2025-01-07 14:03:06 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/hello')
-def hello_name():
-   return 'Hello World!'
+@app.route('/hello/<name>')
+def hello_name(name):
+   return 'Hello %s'%s
 
 if __name__ == '__main__':
    app.run()
